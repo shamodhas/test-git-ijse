203ef4d4d56d59611fb711ac5964686c8ab237b0
shamodhas <shamodha7@gmail.com>
2025-08-25 16:33:05 +0530

    initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * --------------------------------------------
+ * Author: Shamodha Sahan
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.com
+ * --------------------------------------------
+ * Created: 8/25/2025 4:32 PM
+ * Project: untitled
+ * --------------------------------------------
+ **/
+
+public class Test {
+}
